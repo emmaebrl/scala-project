fd8aa377a35e42581ea85abc08624a64359d1947
Tonin RIVORY <toninrvr@MacBook-Air-de-Tonin.local>
2025-04-02 17:53:02 +0200

    tonin is inha house

--- src/main/scala/fr/mosef/scala/template/writer/Writer.scala
+++ src/main/scala/fr/mosef/scala/template/writer/Writer.scala
@@ -11,4 +11,16 @@ class Writer {
       .csv(path)
   }
 
+    def writeCSV(df: DataFrame, outputPath: String): Unit = {
+      df.coalesce(1).write.mode("overwrite").option("header", "true").csv(outputPath)
+    }
+
+    def writeParquet(df: DataFrame, outputPath: String): Unit = {
+      df.write.mode("overwrite").parquet(outputPath)
+    }
+
+    def showPreview(df: DataFrame, numRows: Int = 10): Unit = {
+      df.show(numRows, truncate = false)
+    }
+
 }
