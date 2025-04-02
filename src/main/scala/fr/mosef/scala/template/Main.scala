9955be405ac529b240f2bfa744da3573d5f58c1c
emmaebrl <emmaeberle@hotmail.com>
2025-04-02 18:03:36 +0200

    test

--- src/main/scala/fr/mosef/scala/template/Main.scala
+++ src/main/scala/fr/mosef/scala/template/Main.scala
@@ -1,14 +1,6 @@
 package fr.mosef.scala.template
 import org.apache.spark.sql.SparkSession
-object Main extends App {
-  val sparkSession = SparkSession
-    .builder
-    .master("local[1]")
-    .enableHiveSupport()
-    .getOrCreate()
 
-  print(sparkSession.sql("SELECT 'A'").show())
-}
 import fr.mosef.scala.template.job.Job
 import fr.mosef.scala.template.processor.Processor
 import fr.mosef.scala.template.processor.impl.ProcessorImpl
@@ -20,56 +12,55 @@ import org.apache.spark.SparkConf
 import com.globalmentor.apache.hadoop.fs.BareLocalFileSystem
 import org.apache.hadoop.fs.FileSystem
 
-//object Main extends App with Job {
-//
-//  val cliArgs = args
-//  val MASTER_URL: String = try {
-//    cliArgs(0)
-//  } catch {
-//    case e: java.lang.ArrayIndexOutOfBoundsException => "local[1]"
-//  }
-//  val SRC_PATH: String = try {
-//    cliArgs(1)
-//  } catch {
-//    case e: java.lang.ArrayIndexOutOfBoundsException => {
-//      print("No input defined")
-//      sys.exit(1)
-//    }
-//  }
-//  val DST_PATH: String = try {
-//    cliArgs(2)
-//  } catch {
-//    case e: java.lang.ArrayIndexOutOfBoundsException => {
-//      "./default/output-writer"
-//    }
-//  }
-//
-//  val conf = new SparkConf()
-//  conf.set("spark.driver.memory", "64M")
-//  conf.set("spark.testing.memory", "471859200")
-//
-//  val sparkSession = SparkSession
-//    .builder
-//    .master(MASTER_URL)
-//    .config(conf)
-//    .appName("Scala Template")
-//    .enableHiveSupport()
-//    .getOrCreate()
-//
-//  sparkSession
-//    .sparkContext
-//    .hadoopConfiguration
-//    .setClass("fs.file.impl",  classOf[BareLocalFileSystem], classOf[FileSystem])
-//
-//
-//  val reader: Reader = new ReaderImpl(sparkSession)
-//  val processor: Processor = new ProcessorImpl()
-//  val writer: Writer = new Writer()
-//  val src_path = SRC_PATH
-//  val dst_path = DST_PATH
-//
-//  val inputDF: DataFrame = reader.read(src_path)
-//  val processedDF: DataFrame = processor.process(inputDF)
-//  writer.write(processedDF, "overwrite", dst_path)
-//
-//}
+object Main extends App with Job {
+  val cliArgs = args
+  val MASTER_URL: String = try {
+    cliArgs(0)
+  } catch {
+    case e: java.lang.ArrayIndexOutOfBoundsException => "local[1]"
+  }
+  val SRC_PATH: String = try {
+    cliArgs(1)
+  } catch {
+    case e: java.lang.ArrayIndexOutOfBoundsException => {
+      print("No input defined")
+      sys.exit(1)
+    }
+  }
+  val DST_PATH: String = try {
+    cliArgs(2)
+  } catch {
+    case e: java.lang.ArrayIndexOutOfBoundsException => {
+      "./default/output-writer"
+    }
+  }
+
+  val conf = new SparkConf()
+  conf.set("spark.driver.memory", "64M")
+  conf.set("spark.testing.memory", "471859200")
+
+  val sparkSession = SparkSession
+    .builder
+    .master(MASTER_URL)
+    .config(conf)
+    .appName("Scala Template")
+    .enableHiveSupport()
+    .getOrCreate()
+
+  sparkSession
+    .sparkContext
+    .hadoopConfiguration
+    .setClass("fs.file.impl",  classOf[BareLocalFileSystem], classOf[FileSystem])
+
+
+  val reader: Reader = new ReaderImpl(sparkSession)
+  val processor: Processor = new ProcessorImpl()
+  val writer: Writer = new Writer()
+  val src_path = SRC_PATH
+  val dst_path = DST_PATH
+
+  val inputDF: DataFrame = reader.read(src_path)
+  val processedDF: DataFrame = processor.process(inputDF)
+  writer.write(processedDF, "overwrite", dst_path)
+
+}
--- src/main/scala/fr/mosef/scala/template/writer/Writer.scala
+++ src/main/scala/fr/mosef/scala/template/writer/Writer.scala
@@ -10,5 +10,4 @@ class Writer {
       .mode(mode)
       .csv(path)
   }
-
 }
