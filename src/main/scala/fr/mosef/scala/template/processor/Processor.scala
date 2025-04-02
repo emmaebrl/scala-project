54d1eed9c253621d1626fb88ae6a826702fb5a65
Sharon2607 <chemmamasharon@gmail.com>
2025-04-02 18:17:43 +0200

    template processor

--- src/main/scala/fr/mosef/scala/template/processor/Processor.scala
+++ src/main/scala/fr/mosef/scala/template/processor/Processor.scala
@@ -2,8 +2,11 @@ package fr.mosef.scala.template.processor
 
 import org.apache.spark.sql.DataFrame
 
-trait Processor {"TO THE MOON "
+trait Processor {
 
-  def process(inputDF: DataFrame) : DataFrame
+  def process(inputDF: DataFrame): DataFrame
 
+  def computeSummary(inputDF: DataFrame): DataFrame
+
+  def groupByField(inputDF: DataFrame, field: String): DataFrame
 }
--- src/main/scala/fr/mosef/scala/template/processor/impl/ProcessorImpl.scala
+++ src/main/scala/fr/mosef/scala/template/processor/impl/ProcessorImpl.scala
@@ -1,14 +1,22 @@
 package fr.mosef.scala.template.processor.impl
 
-
 import fr.mosef.scala.template.processor.Processor
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, functions => F}
 
-class ProcessorImpl() extends Processor {
+class ProcessorImpl extends Processor {
 
-  def process(inputDF: DataFrame): DataFrame = {
-    //inputDF.groupBy("group_key").sum("field1")
+  // Rapport 1 : Comptage par champ
+  override def process(inputDF: DataFrame): DataFrame = {
     inputDF.groupBy("group_key").count()
   }
 
+  // Rapport 2 : Statistiques descriptives
+  override def computeSummary(inputDF: DataFrame): DataFrame = {
+    inputDF.describe()
+  }
+
+  // Rapport 3 : Moyenne ou autre aggregation par un champ donné
+  override def groupByField(inputDF: DataFrame, field: String): DataFrame = {
+    inputDF.groupBy(field).agg(F.avg("score").alias("avg_score"))  // adapte "score" si besoin
+  }
 }
